6769fa7a0a5feef97457c5a9905b9eed766ae6a8
AlloTheOne <79634177+AlloTheOne@users.noreply.github.com>
2022-10-26 11:39:06 +0300

    Add files via upload

---
+++ AskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskApp.swift
+//  Ask
+//
+//  Created by Alaa Alabdullah on 26/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct AskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
